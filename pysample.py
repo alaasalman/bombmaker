28cf465db0d00c25892c58d20304e616971caa23
john <john@Cenarius>
2007-09-04 13:08:06 +0300

    Added gtk timer and context menu. Also added the bonbo file.

--- pysample.py
+++ pysample.py
@@ -4,101 +4,53 @@ pygtk.require('2.0')
 
 import gtk
 import gnomeapplet
+import gnome.ui
 import sys
+import os
 import pynotify
-from threading import Timer
+import gobject
 
-timerthread = None
 
-class MyGnomeApplet(gnomeapplet.Applet):
-	def __init__(self, applet, iid):
-		self.applet = applet
-		#self.timerthread = None
-		
-		self.__init_widgets()
-		self.__init_menu()
-		self.__init_callbacks()
-		return None
-		
-	def __init_menu(self):
-		self.menu_xml = """
-      		<popup name="button3">
-         		<menuitem name="Item 1" verb="Help" label="Help"
-                   pixtype="stock" pixname="gtk-help"/>
-         		<menuitem name="Item 2" verb="About" label="About..."
-                   pixtype="stock" pixname="gnome-stock-about"/>
-                <menuitem name="Item 3" verb="Timer" label="Timer"/>
-                <menuitem name="Item 4" verb="Check" label="Check"/>
-      		</popup>
-      	"""
-		self.menu_verbs = [("About", self.on_About), ("Help", None), ("Timer", self.on_Timer_Click), ("Check", self.on_Check)]
-		
-	def __init_callbacks(self):
-		self.applet.connect("button-press-event", self.on_button_press)
-		return None
+def on_Timer_Activate(event, data = None):
+        print "Timer activate"
+        gobject.timeout_add(3000, on_Timer_Expire)
+    
+def on_Timer_Expire():
+    pynotify.init("Shy Applet")
+    notification = pynotify.Notification("Shy Time", "Your Shy is Ready")
+    notification.set_urgency(pynotify.URGENCY_LOW)
+    notification.set_timeout(4000)
+    notification.show()
 
-	def __init_widgets(self):
-		hbox = gtk.HBox()
-		label = gtk.Label("Totem:")
-		hbox.add(label)
-		self.applet.add(hbox)
-		self.applet.show_all()
 
-	def on_button_press(self, widget, event):
-		self.applet.setup_menu(self.menu_xml, self.menu_verbs)
-		
-	def on_About(self, event, data):
-		return None
-		
-	def on_Timer_Click(self, event, data):
-		print "Timer"
-		global timerthread = Timer(10.0, on_Timer_Activate)
-		global timerthread.start()
-		
-	def on_Check(self, event, data):
-		self.timerthread.cancel()
-		print self.timerthread
-			
-		
-	def on_Timer_Activate(self):
-		print "Timer activate"
-		pynotify.init("Tea Applet")
-		notification = pynotify.Notification("TeaTime", "Your Tea is Ready")
-		notification.set_urgency(pynotify.URGENCY_LOW)
-		#notification.set_timeout(4000)
-		notification.show()
-	
-def on_Timer_Activate():
-		print "Timer activate"
-		pynotify.init("Tea Applet")
-		notification = pynotify.Notification("TeaTime", "Your Tea is Ready")
-		notification.set_urgency(pynotify.URGENCY_LOW)
-		#notification.set_timeout(4000)
-		notification.show()
-		
+def on_About_Press(event, data = None):
+    gnome.ui.About("Bomb Applet", "0.1", "GNU General Public License v.2",
+                       "Bomb GNOME applet",
+                       ["Alaa Salman <alaa@codedemigod.com>",],
+                       ).show()
+    
+        
 def sample_factory(applet, iid):
-
-	#label.connect("button-press-event", conn_callback)
-	#applet.connect("button-press-event", conn_callback)
-#	applet.connect("popup-menu", conn_callback)
-	
-	
-#	button = gtk.Button("")
-#	image = gtk.Image()
-#	image.set_from_stock("gtk-media-play",gtk.ICON_SIZE_BUTTON)
-#	button.set_image(image)
-#	hbox.add(button)
-
-#	button = gtk.Button("")
-#	image = gtk.Image()
-#	image.set_from_stock("gtk-media-pause",gtk.ICON_SIZE_BUTTON)
-#	button.set_image(image)
-#	hbox.add(button)
-	MyGnomeApplet(applet, iid)
-	return True
+        
+    image = gtk.Image()
+    image.set_from_file("/home/john/dev/pyapplet/bomb.png")
+    
+    ppmenu_xml = """
+        <popup name="button3">
+            <menuitem name="Timer Item" verb="Timer" label="Timer"/>
+            <menuitem name="About Item" verb="About" stockid="gtk-about"/>
+        </popup>        
+        """
+    ppmenu_verbs = [("About", on_About_Press),("Timer", on_Timer_Activate)]
+            
+    applet.setup_menu(ppmenu_xml, ppmenu_verbs, None)
+    
+    applet.add(image)
+    applet.show_all()
+    return True
 
 def run_in_panel():
-	gnomeapplet.bonobo_factory("OAFIID:GNOME_PysampleApplet_Factory", gnomeapplet.Applet.__gtype__, "hello", "1.0", sample_factory)
+    gnomeapplet.bonobo_factory("OAFIID:GNOME_PysampleApplet_Factory", gnomeapplet.Applet.__gtype__, "hello", "1.0", sample_factory)
 
 def run_in_window():
     main_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
